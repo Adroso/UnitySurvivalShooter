c9f34c8756aebf853cfe1b9f3f64dada83c843b0
Adroso <adrian.lapico@my.jcu.edu.au>
2017-04-08 22:17:52 +1000

    there's an issue with the FindGameObjectWithTag function. Try using GameObject.Find instead. I don't know what this is happening. But i know how to resolve it. Use GameObject.Find instead FindGameObjectWithTag if you make Debug.Log(player.position) and you get always 0,0,0 no matter what position the player really is. You are welcome.﻿

--- Survival Shooter/Assets/Scripts/Enemy/EnemyMovement.cs
+++ Survival Shooter/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -11,7 +11,7 @@ public class EnemyMovement : MonoBehaviour
 
     void Awake ()
     {
-        player = GameObject.FindGameObjectWithTag ("Player").transform;
+        player = GameObject.Find ("Player").transform;
         //playerHealth = player.GetComponent <PlayerHealth> ();
         //enemyHealth = GetComponent <EnemyHealth> ();
         nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
